3390242260f0871d6633aa038eb0756e2836d4dd
AnunayGupta <42909609+AnunayGupta@users.noreply.github.com>
2019-12-22 12:45:41 +0530

    Update Fruits.java

--- Fruits.java
+++ Fruits.java
@@ -8,27 +8,15 @@ public class Fruits
     public Float width ;
     public Float height;
     public Float color_score ;
-    subtype sub ;
-    //public int encode = sub.getvalue() ;
+    String sub1 ;
 
-    enum subtype{
-        GRANNY_SMITH(1) , MANDARIN(2) ,BRAEBURN(3) , GOLDEN_DELICIOUS(4) ,CRIPPS_PINK(5),SPANISH_JUMBO(6),SELECTED_SECONDS(7),
-        TURKEY_NAVEL(8),SPANISH_BELSON(9) ;
-        private int i  ;
-        subtype(int i) {
-            this.i = i ;
-        }
-        public int getvalue(){
-            return this.i ;
-        }
-    }
     public Float distance = Float.MAX_VALUE ;
 
     public void display()
     {
         System.out.println("Fruit name is " + fruit_name);
         System.out.println("Fruit label is " + fruit_label);
-        System.out.println("Fruit subtype is " + sub);
+        System.out.println("Fruit subtype is " + sub1);
         System.out.println("Fruit mass is " + mass);
         System.out.println("Fruit width is " + width);
         System.out.println("Fruit height is " + height);
@@ -42,4 +30,3 @@ public class Fruits
         }
     };
 }
-
